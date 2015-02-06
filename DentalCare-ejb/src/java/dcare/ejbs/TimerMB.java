990325a411b0e8ca5f4e71f97e2066b9d4e77b7a
984221 <984221@CS_LAB113-207.cs.mum.edu>
2015-02-06 10:42:12 -0600

    Adding a timer

---
+++ DentalCare-ejb/src/java/dcare/ejbs/TimerMB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dcare.ejbs;
+
+import java.util.Date;
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author 984221
+ */
+@Stateless
+@LocalBean
+public class TimerMB {
+
+//    @Schedule(minute = "*", second = "*/1", dayOfMonth = "*", month = "*", year = "*", hour = "9-17", dayOfWeek = "Mon-Fri")
+//    
+//    public void myTimer() {
+//        System.out.println("Timer event: " + new Date());
+//    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
